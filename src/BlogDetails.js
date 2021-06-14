bceb7d94bf49f1139ed1f44da27748effa534b2e
mdarif6 <md1arif015@gmail.com>
2021-06-15 01:39:39 +0530

    blog details, useParam

---
+++ src/BlogDetails.js
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+
+export default function BlogDetails() {
+  const { id } = useParams();
+  return (
+    <div className="blog-details">
+      <h2>Blog Details - {id}</h2>
+    </div>
+  );
+}
